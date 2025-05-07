dd84071844085e86e1e6eebe375ca0b028626e04
es10130813 <espinkheart@gmail.com>
2025-05-07 13:05:00 +0900

    Footer 추가 및 css 수정

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'
+    style={{
+        backgroundColor:'#fff',
+        height: '120px', 
+        borderTop: '1px solid #dee2e6',
+        marginTop: '64px',
+      }}
+    ></div>
+  )
+}
