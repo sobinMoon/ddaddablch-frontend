7945730c77369dd389c539eaf6b14f359d2c8f14
sobinMoon <102456745+sobinMoon@users.noreply.github.com>
2025-06-13 00:11:17 +0900

    Update DonationCompleteModal.jsx

--- src/components/DonationCompleteModal.jsx
+++ src/components/DonationCompleteModal.jsx
@@ -5,7 +5,7 @@ import './DonationCompleteModal.css';
 // import defaultImage2 from '../assets/IMG_animal.png';
 // import defaultImage3 from '../assets/IMG_elderly.png';
 // import defaultImage4 from '../assets/IMG_children.png';
-// import defaultImage5 from '../assets/IMG_disabled.png';
+// import defaultImage5 from '../assets/IMG_disabled.png'; 
 // import defaultImage6 from '../assets/IMG_social.jpg';
 import { IoClose } from "react-icons/io5";
 import { createDonationImage } from '../hooks/imageUtils';
